a64b2ef7fd376275715babbd40f6a0a9e1144731
Raphile14 <rllddalangin@gmail.com>
2021-02-24 20:51:17 +0800

    Answer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix is a function that acts as a constructor for a cached matrix from a given matrix
 makeCacheMatrix <- function(x = matrix()) {
+        invVal <- NULL
 
+        ## (Setter) Function that sets the value of x and m that affects the parent
+        set <- function(y) {
+                x <<- y
+                invVal <<- NULL
+        }
+
+        ## (Getter) Function that gets the value of x
+        get <- function() {
+                x
+        }
+
+        ## (Setter) Function that sets invVal to the computed inverted data
+        setInverse <- function(inverse) {
+                invVal <<- mean
+        }
+
+        ## (Getter) Function that returns the inverted data
+        getInverse <- function() {
+                invVal
+        }
+
+        ## Defines the functions available in makeCacheMatrix
+        list(
+                set = set, 
+                get = get,
+                setInverse = setInverse,
+                getInverse = getInverse
+        )
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve is a function that computes and returns the cached and inverted input data
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## Sets the value of val from the getter getInverse function from makeCacheMatrix
+        val <- x$getInverse()
+
+        ## Checks if the cached data is alread inverted. Returns the data from the cache
+        if(!is.null(val)) {
+                message("getting cached data")
+                return(val)
+        }
+
+        ## If data is not yet cached, logic below will execute to obtain and computed inverted data and cache it
+        data <- x$get()
+        val <- solve(data, ...)
+        x$setInverse(val)
+        val
 }
